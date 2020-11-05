e70f08c5e1946ab56f84a5b8ec019395ab933c44
Mikkel Eriksen Klokkerud <mikkelcodes@gmail.com>
2020-11-05 23:42:23 +0100

    tailwind config updated with color profiles
    a Please enter the commit message for your changes. Lines starting

---
+++ packages/website/src/components/layout/Layout.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../../layout.css';
+
+const Layout = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default Layout;
--- packages/website/src/pages/index.js
+++ packages/website/src/pages/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import Layout from '../components/layout/Layout';
 
 const Home = () => {
-  return <div> Test </div>;
+  return (
+    <Layout>
+      <div className="text-white flex items-center justify-center h-screen flex-col">
+        <p className="text-center text-black p-2">Tailwind color profile:</p>
+        <div className="flex">
+          <div className="p-10 h-10 w-10 flex justify-center items-center bg-blue">
+            Blue
+          </div>
+          <div className="p-10 h-10 w-10 flex justify-center items-center bg-navy">
+            Navy
+          </div>
+          <div className="p-10 h-10 w-10 flex justify-center items-center bg-yellow">
+            Yellow
+          </div>
+          <div className="p-10 h-10 w-10 flex justify-center items-center bg-orange">
+            orange
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Home;
