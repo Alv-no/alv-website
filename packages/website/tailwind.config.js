b472a0194e3b9b0537793d018e26ff991b113b57
Mikkel Eriksen Klokkerud <mikkelcodes@gmail.com>
2020-11-05 23:42:23 +0100

    tailwind config updated with color profiles
    a Please enter the commit message for your changes. Lines starting

---
+++ packages/website/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {},
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        navy: '#061838',
+        orange: '#F39123',
+        yellow: '#EABB26',
+        blue: '#1E92D0',
+        offwhite: '#F7F7F7',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
