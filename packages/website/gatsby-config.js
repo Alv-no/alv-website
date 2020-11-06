023d9cf3ee4eb72af835a0be6eca69723ed33a0f
Mikkel Eriksen Klokkerud <mikkelcodes@gmail.com>
2020-11-06 16:10:54 +0100

    Sanity plugin for gatsby

--- packages/website/gatsby-config.js
+++ packages/website/gatsby-config.js
@@ -1,3 +1,19 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
-  plugins: ['gatsby-plugin-postcss'],
+  plugins: [
+    'gatsby-plugin-postcss',
+    {
+      resolve: `gatsby-source-sanity`,
+      options: {
+        projectId: `mnr37rl0`,
+        dataset: `production`,
+        token: process.env.SANITY_TOKEN,
+        graphqlTag: 'default',
+        overlayDrafts: true,
+      },
+    },
+  ],
 };
