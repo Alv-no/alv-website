be1a97bfe290b27bd6db787b39032886f180e45b
Mikkel Eriksen Klokkerud <mikkelcodes@gmail.com>
2020-11-13 12:44:06 +0100

    update footer bg color and responsive spacing

--- packages/website/src/components/footer/index.js
+++ packages/website/src/components/footer/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 export const Footer = ({ ctaText, org, address, email }) => (
-  <footer className="bg-darkblue  md:pt-12 md:pb-10 p-15">
+  <footer className="bg-footerblue  md:pt-12 md:pb-10 p-15">
     <div className="mx-auto max-w-1320 text-center md:text-left flex flex-col md:flex-row items-center md:items-stretch text-white">
-      <div className="text-footer tracking-wider mb-10 md:mb-0">
+      <div className="tracking-wider mb-10 md:mb-0">
         <h4 className="text-lg pb-5 font-bold tracking-wider">CONTACT US</h4>
-        <h5 className="font-bold">ALV AS</h5>
-        <p className="tracking-wider">{org}</p>
-        <p className="">{address}</p>
-        <p className="">{email}</p>
+        <h5 className="text-footer font-bold">ALV AS</h5>
+        <p className="text-footer tracking-wider">{org}</p>
+        <p className="text-footer tracking-wider">{address}</p>
+        <p className="text-footer tracking-wider">{email}</p>
       </div>
       <div className="tracking-wider lg:ml-32 md:ml-15 sm:ml-10 mb-12 md:mb-0">
         <h4 className="text-lg pb-5 font-bold tracking-wider">
@@ -17,10 +17,10 @@ export const Footer = ({ ctaText, org, address, email }) => (
         <p className="text-footer max-w-100 pb-5 tracking-wider">{ctaText}</p>
         <div className="inline-flex">
           <input
-            className="w-50 rounded-full text-sm py-3 px-5 outline-none border border-white bg-darkblue"
+            className="w-50 rounded-full text-sm py-3 px-5 outline-none border border-white bg-footerblue placeholder-white"
             placeholder="Email Address"
           />
-          <button className="ml-6 focus:outline-none text-sm tracking-wide font-bold">
+          <button className="ml-6 focus:outline-none text-sm tracking-wider font-bold">
             SUBSCRIBE
           </button>
         </div>
