97c20ce51e1b51f558d1cc31b09ea8b5c18e567d
Frank Lyder Bredland <fl@nyggen.com>
2020-11-15 20:20:13 +0100

    Created and started styling a Breadcrumb component

--- packages/website/src/components/breadcrumb/index.jsx
+++ packages/website/src/components/breadcrumb/index.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const Breadcrumb = () => <div>Breadcrumb</div>;
+const Breadcrumb = ({ path }) => (
+  <div className="flex space-x-4 text-white">
+    {(path || []).map((item, i) => (
+      <a key={i} href="/" className="last:font-bold">
+        {item}
+      </a>
+    ))}
+  </div>
+);
 
 export default Breadcrumb;
--- packages/website/src/components/breadcrumb/index.stories.jsx
+++ packages/website/src/components/breadcrumb/index.stories.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import Breadcrumb from './index';
 
-export default { title: 'components/breadcrumb' };
+export default {
+  title: 'components/breadcrumb',
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          background: '#061838',
+          paddingTop: '20px',
+          paddingBottom: '20px',
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+};
 
-export const Default = () => <Breadcrumb />;
+export const Default = () => <Breadcrumb path={['Home', 'Blog', 'Tech']} />;
