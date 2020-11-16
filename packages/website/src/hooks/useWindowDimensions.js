5f26b437c4fe8ed8b6517650173ad998a6c60ea4
Mikkel Eriksen Klokkerud <mikkelcodes@gmail.com>
2020-11-16 12:33:21 +0100

    Uncomment window from browser-monads

--- packages/website/src/hooks/useWindowDimensions.js
+++ packages/website/src/hooks/useWindowDimensions.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-// import { window } from 'browser-monads';
+import { window } from 'browser-monads';
 
 function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
