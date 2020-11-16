0d6eaf4e447ecb26e0a6399fd5d9430ce5b7040f
Mikkel Eriksen Klokkerud <mikkelcodes@gmail.com>
2020-11-16 12:16:47 +0100

    Responsive update

--- packages/website/src/components/cta/index.js
+++ packages/website/src/components/cta/index.js
@@ -7,18 +7,18 @@ import image from '../../assets/cta.png';
 export const Cta = ({ internalLink, buttonText, description }) => (
   <section className="text-white mt-10 mb-7">
     <div className="max-w-1200 mx-auto px-5">
-      <div className="flex -mr-35">
+      <div className="flex lg:flex-row flex-col  lg:-mr-35">
         <div className="flex-1">
-          <h2 className="uppercase tracking-wider text-2xl font-bold mb-16">
+          <h2 className="uppercase tracking-wider text-2xl font-bold lg:mb-16 mb-10">
             Join With Us
           </h2>
-          <div className="ml-16">
+          <div className="lg:ml-16 lg:mb-0 mb-10">
             <Description align="left">{description}</Description>
-            <div className="h-12" />
+            <div className="h-8 lg:h-12" />
             <CtaButton>{buttonText}</CtaButton>
           </div>
         </div>
-        <div className="flex-1 pl-15 ">
+        <div className="flex-1 lg:pl-15 ">
           <Link to={internalLink}>
             <img
               src={image}
