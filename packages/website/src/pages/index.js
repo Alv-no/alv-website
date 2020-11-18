e91bc52ba92978890b0766b37a89e3d5adc38579
Mikkel Eriksen Klokkerud <mikkelcodes@gmail.com>
2020-11-18 22:20:20 +0100

    footer component fully responsive

--- packages/website/src/pages/index.js
+++ packages/website/src/pages/index.js
@@ -11,15 +11,17 @@ const Employees = () => {
   return (
     <Layout>
       {/* Navy background for illustrative purposes */}
-      <div className="bg-navy w-full pt-8 sm:pt-16 pb-12">
+      <div className="bg-navy w-full pt-8 sm:pt-16 sm:pb-12 pb-4">
         <Title>Ansatte</Title>
         <div className="sm:h-3 h-4" />
         <div className="flex justify-center">
-          <Description align="center">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam.
-          </Description>
+          <span className="px-6">
+            <Description align="center">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam.
+            </Description>
+          </span>
         </div>
         <div className="h-10 sm:h-16 md:h-24 mt-3" />
         <Tags
