8f0e7d80fd03f37b2349bf5e9c5ec63ab7ee377d
Mikkel Eriksen Klokkerud <mikkelcodes@gmail.com>
2020-11-18 18:04:24 +0100

    finishing touches on CTA mobile/tablet

--- packages/website/src/components/cta/index.js
+++ packages/website/src/components/cta/index.js
@@ -5,25 +5,28 @@ import { CtaButton } from '../ctaButton';
 import image from '../../assets/cta.png';
 
 export const Cta = ({ internalLink, buttonText, description }) => (
-  <section className="text-white mt-10 mb-7 ">
+  <section className="text-white mt-12 mb-7">
     <div className="max-w-1200 mx-auto lg:px-5">
       <div className="flex lg:flex-row flex-col-reverse xl:-mr-35">
-        <div className="flex-1">
-          <h2 className="uppercase tracking-wider text-2xl font-bold lg:mb-16 mb-10">
+        <div className="lg:flex-1 px-6 lg:px-0">
+          <h2 className="uppercase tracking-wider text-2xl font-bold sm:mb-15 lg:mb-16 mb-10">
             Join With Us
           </h2>
-          <div className="lg:ml-16 lg:mb-0 mb-10">
+          <div className="xl:ml-16 lg:ml-0 sm:ml-18 lg:mb-0 mb-10">
             <Description align="left">{description}</Description>
             <div className="h-8 lg:h-12" />
-            <CtaButton>{buttonText}</CtaButton>
+            <div className="w-full flex justify-between sm:block">
+              <div />
+              <CtaButton>{buttonText}</CtaButton>
+            </div>
           </div>
         </div>
         <Link to={internalLink}>
-          <div className="flex-1 lg:pl-12 sm:h-100 xs:h-70 h-54 overflow-hidden ">
+          <div className="lg:flex-1 lg:pl-12 w-full">
             <img
               src={image}
               alt="cta"
-              className="object-cover lg:transform lg:ml-3 lg:scale-95"
+              className="object-cover lg:transform h-54 sm:h-100 mb-10 w-full lg:w-auto lg:ml-3 lg:scale-95"
             />
           </div>
         </Link>
