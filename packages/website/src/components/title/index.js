fd875fd115cfe10c11ef87c5b1aaf1ea196f356f
Frank Lyder Bredland <fl@nyggen.com>
2020-11-23 08:57:26 +0100

    Added title on the frontpage

--- packages/website/src/components/title/index.js
+++ packages/website/src/components/title/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import classnames from 'classnames';
 
-const Dot = () => <span className="text-yellow">.</span>;
+const Dot = () => <span className="text-yellow font-bold">.</span>;
 
-export const Title = ({ children }) => (
-  <h2 className="text-white w-full text-center text-4xl py-4 font-bold tracking-wider uppercase">
-    {children}
-    <Dot />
-  </h2>
-);
+export const Title = ({ children, bold = true }) => {
+  const classNames = classnames(
+    'text-white w-full text-center text-4xl py-4 tracking-wider uppercase',
+    bold ? 'font-bold' : 'font-thin'
+  );
+
+  return (
+    <h2 className={classNames}>
+      {children}
+      <Dot />
+    </h2>
+  );
+};
