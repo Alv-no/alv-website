312064d7d54a93c1f76a36f58f826c9d6f402f5d
Frank Lyder Bredland <fl@nyggen.com>
2020-11-23 08:57:26 +0100

    Added title on the frontpage

--- packages/website/src/pages/index.js
+++ packages/website/src/pages/index.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import Layout from '../components/layout';
 import { Title } from '../components/title';
-import { Description } from '../components/description';
 
 const Index = () => {
   return (
     <Layout>
       <div className="bg-navy w-full pt-8 sm:pt-16 sm:pb-12 pb-4 h-screen">
-        <Title>Index</Title>
+        <Title bold={false}>
+          <div>Vi bygger </div>
+          <div className="font-black">Norges mest attraktive</div>
+          <span>konsulentselskap</span>
+        </Title>
         <div className="sm:h-3 h-4" />
-        <div className="flex justify-center">
-          <span className="px-6">
-            <Description align="center">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam.
-            </Description>
-          </span>
-        </div>
+        <div className="flex justify-center"></div>
       </div>
     </Layout>
   );
