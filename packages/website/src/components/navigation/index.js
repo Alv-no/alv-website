1de4b8b828b50386d22287ad51c984b8c0c4f83d
Mikkel Eriksen Klokkerud <mikkelcodes@gmail.com>
2020-11-25 02:22:51 +0100

    add responsiveness to laptop/desktop navigation

--- packages/website/src/components/navigation/index.js
+++ packages/website/src/components/navigation/index.js
@@ -17,7 +17,7 @@ const Subtitle = ({ children, margin }) => (
 );
 
 const ListLink = ({ children, link }) => (
-  <li className="text-base uppercase font-light mb-4 tracking-wider">
+  <li className="text-base uppercase font-light nine:mb-3 2xl:mb-4 tracking-wider">
     <Link to={link}>{children}</Link>
   </li>
 );
@@ -25,11 +25,11 @@ const ListLink = ({ children, link }) => (
 export const Navigation = ({ open, toggleClose }) => {
   return (
     <header
-      className={`text-white tracking-wider absolute z-50 h-full w-full bg-navy p-8 top-0 transition duration-300 ${
+      className={`text-white tracking-wider fixed z-50 h-full w-full bg-navy p-8 top-0 transition duration-300 ${
         open ? 'opacity-100' : 'opacity-0 pointer-events-none'
       }`}
     >
-      <div className="flex justify-between mx-auto auto mb-25 max-w-1200 items-center">
+      <div className="flex justify-between mx-auto auto nine:mb-10 2xl:mb-25 max-w-1200 items-center">
         <div className="w-32" />
         <span className="">
           <Link to="/">
@@ -46,8 +46,8 @@ export const Navigation = ({ open, toggleClose }) => {
       </div>
       <div className="flex justify-between max-w-1000 mx-auto">
         <div>
-          <LargeLink margin="mb-12">Vi Tilbyr</LargeLink>
-          <Subtitle margin="mb-10">Systemutvikling</Subtitle>
+          <LargeLink margin="nine:mb-10 2xl:mb-12">Vi Tilbyr</LargeLink>
+          <Subtitle margin="nine:mb-8 2xl:mb-10">Systemutvikling</Subtitle>
           <ul className="">
             <ListLink link="/">.Net Utvikler</ListLink>
             <ListLink link="/">Java Utvikler</ListLink>
@@ -61,8 +61,10 @@ export const Navigation = ({ open, toggleClose }) => {
           <Subtitle margin="mt-7">Digitalisering</Subtitle>
         </div>
         <div className="transform translate-x-5">
-          <Subtitle margin="mt-20 pt-1">Data &amp; Analyse</Subtitle>
-          <ul className="mt-10">
+          <Subtitle margin="nine:mt-16 2xl:mt-20 nine:pt-3 2xl:pt-1">
+            Data &amp; Analyse
+          </Subtitle>
+          <ul className="nine:mt-8 2xl:mt-10">
             <ListLink link="/">Data Rådivning/Strategi</ListLink>
             <ListLink link="/">Business Intelligence</ListLink>
             <ListLink link="/">Maskinlæring &amp; AI</ListLink>
@@ -70,27 +72,27 @@ export const Navigation = ({ open, toggleClose }) => {
             <ListLink link="/">Opplæring</ListLink>
           </ul>
           <Subtitle margin="mt-12">Informasjonssikkerhet</Subtitle>
-          <ul className="mt-10">
+          <ul className="nine:mt-8 2xl:mt-10">
             <ListLink link="/">Sikkerhetstesting</ListLink>
             <ListLink link="/">Cloud Security</ListLink>
           </ul>
         </div>
         <div>
-          <LargeLink margin="mb-12">Jobbe i Alv</LargeLink>
-          <LargeLink margin="mb-12">Selskapet</LargeLink>
-          <Subtitle margin="mb-6">Om Oss</Subtitle>
-          <Subtitle margin="mb-6">Aktuelt</Subtitle>
-          <Subtitle margin="mb-6">
+          <LargeLink margin="nine:mb-10 2xl:mb-12">Jobbe i Alv</LargeLink>
+          <LargeLink margin="nine:mb-10 2xl:mb-12">Selskapet</LargeLink>
+          <Subtitle margin="nine:mb-5 2xl:mb-6">Om Oss</Subtitle>
+          <Subtitle margin="nine:mb-5 2xl:mb-6">Aktuelt</Subtitle>
+          <Subtitle margin="nine:mb-5 2xl:mb-6">
             <Link to="/ansatte">Ansatte</Link>
           </Subtitle>
-          <LargeLink margin="mt-12">Videoserie</LargeLink>
-          <LargeLink margin="mt-12">Blogg</LargeLink>
-          <LargeLink margin="mt-12">
+          <LargeLink margin="nine:mt-10 2xl:mt-12">Videoserie</LargeLink>
+          <LargeLink margin="nine:mt-10 2xl:mt-12">Blogg</LargeLink>
+          <LargeLink margin="nine:mt-10 2xl:mt-12">
             <Link to="kontakt-oss">Kontakt Oss</Link>
           </LargeLink>
         </div>
       </div>
-      <div className="flex justify-center mt-24">
+      <div className="flex justify-center nine:mt-10 2xl:mt-24">
         <span className="cursor-pointer" onClick={toggleClose}>
           <Icon.Cross />
         </span>
