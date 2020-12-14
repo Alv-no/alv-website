3d1b0051dc0da26c9001408c2162bade66c684b9
Mikkel Eriksen Klokkerud <mikkelcodes@gmail.com>
2020-12-14 14:49:41 +0100

    replace tagbar component with employeeFilter

---
+++ packages/website/src/components/employeeFilter/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import * as Icon from '../icon';
+import { Checkbox } from '../checkbox';
+
+export const EmployeeFilter = ({ allTags, onChange }) => (
+  <div className="mx-auto flex md:flex-row flex-col text-white sm:items-center justify-center px-4 sm:px-4 mb-10">
+    <div className="flex mr-4 text-base tracking-wider font-bold mb-5 sm:mb-3">
+      Kategorier{' '}
+      <span className="transform scale-10 -translate-y-2px h-5 -mx-32">
+        <Icon.Tag />
+      </span>
+    </div>
+    <div className="flex -ml-4 flex-wrap items-center">
+      {allTags.map((sanityTag) => {
+        return (
+          <Checkbox
+            onChange={onChange}
+            key={sanityTag.node.id}
+            {...sanityTag.node}
+          >
+            {sanityTag.node.tag}
+          </Checkbox>
+        );
+      })}
+    </div>
+  </div>
+);
