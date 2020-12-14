a42933fee558bc62339c268b05234f3f423f1e48
Mikkel Eriksen Klokkerud <mikkelcodes@gmail.com>
2020-12-14 14:51:40 +0100

    responsive blog overview

--- packages/website/src/components/introContainer/index.js
+++ packages/website/src/components/introContainer/index.js
@@ -3,12 +3,14 @@ import { FeaturedCard } from '../featuredCard';
 
 export const IntroContainer = ({ article, children, fallbackImg }) => {
   return (
-    <div className="bg-navy w-full pt-5 px-5">
-      <div className="max-w-1200 mx-auto flex">
-        <div className="flex-1">
-          <div className="pl-8 pb-10 mr-10 lg:pr-32 pt-20">{children}</div>
+    <div className="bg-navy w-full pt-5 nine:px-10 twelve:pl-4">
+      <div className="max-w-1200 mx-auto flex twelve:flex-row flex-col nine:justify-center twelve:justify-start">
+        <div className="twelve:flex-1">
+          <div className="twelve:pl-8 twelve:pb-10 twelve:mr-10 twelve:pr-32 twelve:pt-20 text-center twelve:text-left">
+            {children}
+          </div>
         </div>
-        <div className="flex-1 pr-4 pl-2">
+        <div className="twelve:flex-1 twelve:pr-4 twelve:pl-2 twelve:w-auto eight:w-2/3 w-full twelve:mx-0 mx-auto">
           <div className="transform translate-y-16">
             <FeaturedCard {...article} fallbackImg={fallbackImg} />
           </div>
