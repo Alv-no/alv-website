798728cf9556a4aac21e3c8ef81fe0d283e67f50
Mikkel Eriksen Klokkerud <mikkelcodes@gmail.com>
2020-12-15 04:38:50 +0100

    make breadcrumb dynamically generate content and internalLinks

--- packages/website/src/components/breadcrumb/index.jsx
+++ packages/website/src/components/breadcrumb/index.jsx
@@ -1,16 +1,43 @@
 import React from 'react';
+import Link from 'gatsby-link';
 
-const Breadcrumb = ({ path }) => (
-  <div className="overflow-hidden">
-    <div className="flex space-x-4 text-white -ml-4 tracking-wider">
-      {(path || []).map((item, i) => (
-        <a key={i} href="/" className="last:font-bold font-thin">
-          <span className="mr-3 -ml-1">/</span>
-          {item}
-        </a>
-      ))}
+const Breadcrumb = ({ path }) => {
+  let crumbs = [];
+  const filteredPath = path.filter((el) => el.length > 0);
+  if (filteredPath) {
+    crumbs = filteredPath.map((el) => {
+      let formattedEl = el[0].toUpperCase() + el.slice(1);
+      if (formattedEl.includes('-')) {
+        formattedEl = formattedEl.split('-').join(' ');
+      }
+
+      return formattedEl;
+    });
+  }
+  crumbs.unshift('Hjem');
+  return (
+    <div className="overflow-hidden">
+      {crumbs.length > 1 && (
+        <div className="flex space-x-4 text-white -ml-4 tracking-wider">
+          {(crumbs || []).map((item, i) => {
+            let link;
+            if (i === 0) {
+              link = '/';
+            } else {
+              link = '/' + path.slice(0, i).join('/');
+            }
+
+            return (
+              <Link to={link} key={i} className="last:font-bold font-thin">
+                <span className="mr-3 -ml-1 font-light">/</span>
+                {item}
+              </Link>
+            );
+          })}
+        </div>
+      )}
     </div>
-  </div>
-);
+  );
+};
 
 export default Breadcrumb;
