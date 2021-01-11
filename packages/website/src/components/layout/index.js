a6a88af7cf3fb2b35fc381fabe6824afcd76a74a
Mikkel Eriksen Klokkerud <mikkelcodes@gmail.com>
2021-01-11 12:24:19 +0100

    working on slider, add fade, typewriter and SEO

--- packages/website/src/components/layout/index.js
+++ packages/website/src/components/layout/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Footer } from '../footer';
 import { Header, MobileHeader } from '../header';
+import { SEO } from '../seo';
 import '../../layout.css';
 
 const Layout = ({ children, path }) => {
   return (
     <>
+      <SEO />
       <Header path={path} />
       <MobileHeader />
       <div>{children}</div>
--- packages/website/src/pages/index.js
+++ packages/website/src/pages/index.js
@@ -4,10 +4,13 @@ import { Hero } from '../components/hero';
 import { Subtitle } from '../components/title';
 import { BlogSlider } from '../components/blogSlider';
 import { CtaButton } from '../components/ctaButton';
-import * as Button from '../components/button';
+import { WhoWeAre } from '../components/whoWeAre';
+import { OwnedByStaff } from '../components/ownedByStaff';
+import { OurServices } from '../components/ourServices';
 import BackgroundImage from 'gatsby-background-image-es5';
 import { useIndexQuery } from '../hooks/useIndexQuery';
 import video from '../assets/heroVideo.webm';
+import Fade from 'react-reveal/Fade';
 
 const Index = () => {
   const data = useIndexQuery();
@@ -27,47 +30,39 @@ const Index = () => {
             <div style={{ height: '550px' }} className="relative">
               <iframe
                 className="w-full h-full"
-                src={video}
+                src={`${video}?controls=0`}
                 title="Introduksjon"
                 frameBorder="0"
                 webkitallowfullscreen="true"
                 mozallowfullscreen="true"
                 allowFullScreen
+                controls="0"
+                volume="0"
+                autoplay="true"
               />
-              <h2 className="uppercase absolute top-0 right-0 text-slider font-semibold tracking-wider text-white w-4/12 transform translate-x-20 leading-tight -mr-40 mt-40">
-                Sed ut perspici atis unde omnis iste natus.
-              </h2>
+              <Fade>
+                <h2 className="uppercase absolute top-0 right-0 text-slider font-semibold tracking-wider text-white w-4/12 transform translate-x-20 leading-tight -mr-40 mt-40">
+                  Sed ut perspici atis unde omnis iste natus.
+                </h2>
+              </Fade>
             </div>
           </div>
-          <div
-            className="px-12 grid text-white mt-20 tracking-wider gap-x-10 max-w-1440 mx-auto"
-            style={{ gridTemplateColumns: '1fr 1fr' }}
-          >
-            <div className="flex justify-end w-full">
-              <div>
-                <h4 className="uppercase font-semibold text-about mt-2">
-                  Hvem er vi
-                </h4>
-                <div className="w-10 h-2px bg-yellow mt-2" />
-              </div>
-            </div>
-            <div>
-              <p className="text-xl tracking-wider mb-8 font-thin">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam.
-              </p>
-              <p className="text-xl tracking-wider mb-8 font-thin">
-                Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat. Duis aute irure dolor in reprehenderit in
-                voluptate velit esse cillum.
-              </p>
-              <Button.CtaArrow>Les mer</Button.CtaArrow>
-            </div>
-          </div>
+          <WhoWeAre title="Hvem er vi">
+            {' '}
+            <p className="text-xl tracking-wider mb-8 font-thin">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam.
+            </p>
+            <p className="text-xl tracking-wider mb-8 font-thin">
+              Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat. Duis aute irure dolor in reprehenderit in
+              voluptate velit esse cillum.
+            </p>
+          </WhoWeAre>
         </div>
         {/* CTA STARTS HERE */}
-        <div className="bg-navy w-full pt-15 pb-5 text-white">
+        <div className="bg-navy w-full 2xl:pt-15 pb-5 text-white">
           <div
             className="pr-12 grid max-w-1440 mx-auto"
             style={{ gridTemplateColumns: '50% auto' }}
@@ -78,100 +73,31 @@ const Index = () => {
                 style={{ height: '715px' }}
               >
                 <div className="-mr-64 w-6/12">
-                  <h2 className="uppercase text-slider mb-8 font-semibold tracking-wider text-white ">
-                    Sed ut perspici atis unde omnis iste natus.
-                  </h2>
-                  <p className="text-xl tracking-wider mb-12 font-light">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut.
-                  </p>
-                  <div className="w-full flex justify-end">
-                    <div className="-mr-40">
-                      <CtaButton>Hyr en alv</CtaButton>
+                  <Fade>
+                    <h2 className="uppercase text-slider mb-8 font-semibold tracking-wider text-white ">
+                      Sed ut perspici atis unde omnis iste natus.
+                    </h2>
+                    <p className="text-xl tracking-wider mb-12 font-light">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut.
+                    </p>
+                    <div className="w-full flex justify-end">
+                      <div className="-mr-40">
+                        <CtaButton>Hyr en alv</CtaButton>
+                      </div>
                     </div>
-                  </div>
+                  </Fade>
                 </div>
               </div>
             </BackgroundImage>
           </div>
         </div>
+        <div className="bg-navy h-32" />
         {/* SERVICES STARTS HERE */}
-
-        <div
-          className="max-w-1440 mx-auto grid gap-x-10 pt-10 bg-navy text-white"
-          style={{
-            gridTemplateColumns: '1fr 1fr',
-          }}
-        >
-          <div className="w-full"></div>
-          <div className="font-light flex flex-col justify-between h-full text-xl">
-            <div>
-              <Subtitle>Våre tjenester</Subtitle>
-              <p className="mb-4 mt-4 pr-15 w-full pr-20">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut.
-              </p>
-              <Button.CtaArrow>Les mer</Button.CtaArrow>
-              <div className="h-12" />
-            </div>
-          </div>
-        </div>
-        <div
-          className="max-w-1440 mx-auto grid gap-x-10 pb-20 bg-navy text-white"
-          style={{
-            gridTemplateColumns: '1fr 1fr',
-          }}
-        >
-          <div className="w-full pl-12">
-            <ul>
-              <li className="text-nav tracking-wider font-semibold mb-8">
-                <p className="uppercase">Systemutvikling</p>
-                <div className="w-12 mt-2 mb-12 h-2px bg-yellow" />
-                <div className="pl-10 mb-15">
-                  <p className="tracking-wider text-lg leading-snug font-thin mb-4">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incidunt.
-                  </p>
-                  <div className="flex -mr-24 z-40 relative items-center cursor-pointer">
-                    <p className="font-semibold tracking-wider uppercase text-base w-54">
-                      Find out more
-                    </p>
-                    <div className="h-2px bg-white w-full" />
-                  </div>
-                </div>
-              </li>
-              <li className="uppercase text-nav tracking-wider font-semibold mb-8">
-                Prosjektledelse
-              </li>
-              <li className="uppercase text-nav tracking-wider font-semibold mb-8">
-                Digitalisering
-              </li>
-              <li className="uppercase text-nav tracking-wider font-semibold mb-8">
-                Data & Analyse
-              </li>
-              <li className="uppercase text-nav tracking-wider font-semibold mb-8">
-                Informasjonssikkerhet
-              </li>
-            </ul>
-          </div>
-          <div className="font-light flex flex-col justify-between h-full text-xl">
-            <div>
-              <BackgroundImage
-                fluid={data.services.childImageSharp.fluid}
-                style={{ height: '530px' }}
-              >
-                <div
-                  className="flex justify-end items-center p-16 bg-black bg-opacity-50"
-                  style={{ height: '530px' }}
-                >
-                  <div className="text-slider uppercase font-semibold tracking-wider leading-tighter w-5/6 text-right">
-                    Quis autem vel eum iure reprehende
-                  </div>
-                </div>
-              </BackgroundImage>
-            </div>
-          </div>
-        </div>
+        <OurServices image={data.services.childImageSharp.fluid}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut.
+        </OurServices>
         {/* CTA2 STARTS HERE */}
         <div className="bg-navy w-full pt-24 pb-20">
           <div
@@ -179,22 +105,26 @@ const Index = () => {
             style={{ gridTemplateColumns: '60% auto' }}
           >
             <div className="pr-20 w-5/6">
-              <h2 className="text-5xl font-light mb-7 uppercase">
-                Bli en av oss
-              </h2>
-              <p className="mb-4 pr-15 w-full font-light text-xl mb-15">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam.
-              </p>
+              <Fade>
+                <h2 className="text-5xl font-light mb-7 uppercase">
+                  Bli en av oss
+                </h2>
+                <p className="mb-4 pr-15 w-full font-light text-xl mb-15">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam.
+                </p>
+              </Fade>
               <div className="w-full flex justify-end">
                 <div className="mb-20">
-                  <CtaButton>Hyr en alv</CtaButton>
+                  <Fade>
+                    <CtaButton>Hyr en alv</CtaButton>
+                  </Fade>
                 </div>
               </div>
               <div className="h-10 relative" />
               <div
-                className="absolute tracking-wider leading-tight inset-x-0 text-white text-3xl font-semibold w-3/12 text-right z-40 uppercase"
+                className="absolute tracking-wider leading-tight inset-x-0 text-white text-3xl font-semibold w-7/12 lg:w-5/12 2xl:w-3/12 text-right z-40 uppercase"
                 style={{ left: '42%', right: '50%' }}
               >
                 Ut enim ad minima veniam, quis nostrum exerciE tationem ullam
@@ -210,8 +140,8 @@ const Index = () => {
         </div>
         {/* BLOGG SLIDER STARTS HERE */}
         <div className="w-full bg-navy">
-          <div className="max-w-1440 mx-auto">
-            <div className="px-12 -mb-7">
+          <div className="">
+            <div className="mx-12 -mb-7 max-w-1440 mx-auto">
               <Subtitle>Hva tenker alvene?</Subtitle>
             </div>
             <BlogSlider
@@ -222,49 +152,11 @@ const Index = () => {
           </div>
         </div>
         {/* OWNED BY STAFF SECTION */}
-        <div className="bg-darkblue bg-opacity-50 w-full text-white tracking-wider py-20 mt-15">
-          <div
-            className="max-w-1440 mx-auto px-12 grid gap-x-25"
-            style={{ gridTemplateColumns: '30% auto' }}
-          >
-            <div className="">
-              <h3 className="text-about-xl font-light mb-10 uppercase">
-                Eid av våre ansatte
-              </h3>
-              <p className="font-light text-xl">
-                Alv AS is 100% owned by our employees. This means that you will
-                have the opportunity to buy into the ownership side of the
-                company when you are hired.
-              </p>
-            </div>
-            <div className="flex h-full items-center">
-              <div className="w-64 h-64 bg-black rounded-full mr-15">
-                {/* Image */}
-              </div>
-              <div className="flex">
-                <div className="tracking-wider border-r-2 pr-6">
-                  <div className="text-slider font-semibold mb-10">50%</div>
-                  <div className="text-slider font-semibold mb-10">25%</div>
-                  <div className="text-slider font-semibold">25%</div>
-                </div>
-                <div className="text-lg font-light tracking-wider w-64 pl-6">
-                  <p className="mb-9 leading-snug">
-                    <span className="font-semibold">Reinvestert</span> tilbake i
-                    selskapet
-                  </p>
-                  <p className="mb-9 leading-snug">
-                    <span className="font-semibold">Fordelt</span> utover
-                    ansatte i form av bonus
-                  </p>
-                  <p className="leading-snug">
-                    <span className="font-semibold">Tilgjengelig</span> som
-                    utbytte for eiere
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <OwnedByStaff>
+          Alv AS is 100% owned by our employees. This means that you will have
+          the opportunity to buy into the ownership side of the company when you
+          are hired.
+        </OwnedByStaff>
       </div>
     </Layout>
   );
