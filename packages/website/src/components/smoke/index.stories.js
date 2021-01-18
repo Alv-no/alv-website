9f1a934436ec39d3ac420ecacbbf9a1c2aca9384
Frank Lyder Bredland <fl@nyggen.com>
2021-01-18 17:30:57 +0100

    Creating a smoke animation

---
+++ packages/website/src/components/smoke/index.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Smoke from './index';
+
+export default {
+  title: 'components/smoke',
+};
+
+export const Default = ({ ...args }) => <Smoke {...args} />;
+Default.args = {};
