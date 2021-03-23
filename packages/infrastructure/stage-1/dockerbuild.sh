56bc4bcd7b41718a19f0f1de5f8308416087a413
Frank Lyder Bredland <fl@nyggen.com>
2021-03-23 09:26:54 +0100

    Fix build

--- packages/infrastructure/stage-1/dockerbuild.sh
+++ packages/infrastructure/stage-1/dockerbuild.sh
@@ -4,6 +4,6 @@ USERNAME="$2"
 PASSWORD="$3"
 NEW_IMAGE_NAME="$1/website:$4"
 
-docker build -f ../../../website.Dockerfile -t website ../../../
+docker build -f ../../../website.Dockerfile -t website --no-cache ../../../
 docker tag website $NEW_IMAGE_NAME
 echo $PASSWORD|docker login $1 --username $USERNAME --password-stdin&& docker push $NEW_IMAGE_NAME
