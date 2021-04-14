572cde3f9a90b6b96842595b26e1ce8b4393546b
Mikkel Eriksen Klokkerud <mikkel@mac.home>
2021-04-14 15:34:24 +0200

    fix text overlapping - #301

--- packages/cms/schemas/categoryPage.js
+++ packages/cms/schemas/categoryPage.js
@@ -37,11 +37,6 @@ export default {
       type: 'string',
     },
     {
-      name: 'headingSplit',
-      title: 'Split Heading',
-      type: 'splitWord',
-    },
-    {
       name: 'heroDescription',
       title: 'Hero Description',
       type: 'string',
--- packages/website/src/components/navyIntro/index.js
+++ packages/website/src/components/navyIntro/index.js
@@ -3,23 +3,16 @@ import BackgroundImage from 'gatsby-background-image-es5';
 import { Title } from '../title';
 import { Description } from '../description';
 
-export const NavyIntro = ({ title, description, image, headingSplit }) => (
+export const NavyIntro = ({ title, description, image }) => (
   <>
     {image ? (
       <div className="sm:z-40 -mt-35 -mb-2">
         <BackgroundImage fluid={image}>
-          <div className="bg-navy bg-opacity-75 w-full pt-10 sm:h-50vh h-screen flex flex-col justify-center items-center sm:pt-16 sm:pb-16 pb-4">
+          <div className="bg-navy bg-opacity-75 w-full px-5 pt-10 sm:h-620 h-screen flex flex-col justify-center items-center sm:pt-16 sm:pb-16 pb-4">
             <Title>
-              <span
-                className={`${
-                  headingSplit && 'hidden'
-                } xs:inline sm:text-3xl lg:text-4xl`}
-              >
+              <span className="xs:inline sm:text-3xl lg:text-4xl hyphenate">
                 {title}
               </span>
-              {headingSplit && (
-                <span className="xs:hidden">{headingSplit}</span>
-              )}
             </Title>
             <div className="sm:h-6 h-4" />
             <div className="flex justify-center">
@@ -34,14 +27,9 @@ export const NavyIntro = ({ title, description, image, headingSplit }) => (
     ) : (
       <div className="bg-navy w-full pt-10 sm:h-auto h-screen sm:block flex justify-center items-center flex-col sm:pt-16 sm:pb-35 pb-4 overflow-hidden">
         <Title>
-          <span
-            className={`${
-              headingSplit && 'hidden'
-            } xs:inline sm:text-3xl lg:text-4xl`}
-          >
+          <span className="xs:inline sm:text-3xl lg:text-4xl hyphenate">
             {title}
           </span>
-          {headingSplit && <span className="xs:hidden">{headingSplit}</span>}
         </Title>
         <div className="sm:h-8 h-4" />
         <div className="flex justify-center">
