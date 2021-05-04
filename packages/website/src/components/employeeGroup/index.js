fd167fe0d36fd0f1179c1a8b53712306c717593a
Mikkel Eriksen Klokkerud <mikkel@mac.home>
2021-05-05 00:30:05 +0200

    make employee descriptions crawable

--- packages/website/src/components/employeeGroup/index.js
+++ packages/website/src/components/employeeGroup/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PortableText from '@sanity/block-content-to-react';
 import { EmployeeCard } from '../employeeCard';
 import { EmployeeBio } from '../employeeBio';
 
@@ -35,13 +36,24 @@ export const EmployeeGroup = ({
           } grid xs:gap-4 justify-center xs:mx-auto mb-4 xs:max-w-570 seven:max-w-none nine:max-w-grid -mx-4 xs:mx-0 grid-cols-employees-xs xs:grid-cols-employees-sm seven:grid-cols-employees-md nine:grid-cols-employees-lg`}
         >
           {group.map((employee) => (
-            <EmployeeCard
-              {...employee}
-              handleClick={handleCardClick}
-              key={employee.id}
-              activeBio={activeBio}
-              fallbackImg={fallbackImg}
-            />
+            <>
+              <EmployeeCard
+                {...employee}
+                handleClick={handleCardClick}
+                key={employee.id}
+                activeBio={activeBio}
+                fallbackImg={fallbackImg}
+              />
+              <p className="absolute opacity-0 pointer-events-none">
+                {employee._rawBio && (
+                  <PortableText
+                    blocks={employee._rawBio}
+                    projectId="mnr37rl0"
+                    dataset="production"
+                  />
+                )}
+              </p>
+            </>
           ))}
         </div>
         {activeBio && group.some((el) => el.id === activeBio.id) ? (
