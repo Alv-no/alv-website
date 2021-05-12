412774e3c0eebcfc799d28fb7bfd4b9df54df763
Mikkel Eriksen Klokkerud <mikkel@mac.home>
2021-05-12 05:51:50 +0200

    issue-347

--- packages/website/src/components/videoHero/index.js
+++ packages/website/src/components/videoHero/index.js
@@ -10,26 +10,27 @@ export const VideoHero = ({
   fallbackImg,
   video,
   playlist,
+  description,
+  title,
 }) => {
   return (
     <div className="bg-navy w-full pt-2 twelve:pb-10 -mt-5 mb-30 twelve:mb-0">
       <div className="twelve:grid grid-cols-videos-hero mx-auto twelve:mb-24">
         <div />
         <span
-          className={`relative twelve:-ml-20 twelve:pr-40 2xl:ml-0 2xl:pr-0 transform sm:translate-x-2/7 twelve:translate-x-0 block ${styles.card}`}
+          className={`relative twelve:-ml-20 twelve:pr-40 2xl:ml-0 2xl:pr-0 transform md:translate-x-2/7 twelve:translate-x-0 block ${styles.card}`}
         >
           <Image
             fluid={backgroundImage}
-            className={`-mx-12 h-screen sm:mx-0 transform scale-130 twelve:translate-x-10 -translate-y-5  ${styles.bgImage}`}
+            className={`-mx-12 h-screen lg:mx-0 transform scale-130 twelve:translate-x-10 -translate-y-5  ${styles.bgImage}`}
           />
           <div
-            className={`absolute -translate-y-32 w-full text-white top-1/2 transform sm:-translate-y-1/2 sm:-translate-x-40 md:m-0 sm:ml-15 ${styles.cta}`}
+            className={`absolute -translate-y-32 w-full text-white top-1/2 transform sm:-translate-y-1/2 md:-translate-x-40 md:m-0 sm:ml-15  ${styles.cta}`}
           >
-            <Title classes="sm:text-left text-center w-full">Videoserie</Title>
+            <Title classes="sm:text-left text-center w-full">{title}</Title>
             <div className="pt-7 sm:pt-9 sm:ml-10">
               <h2 className="sm:text-blog text-center text-xl sm:text-left mx-auto sm:mx-0 sm:w-100 sm:pr-12 w-5/6 tracking-wider">
-                Se gjerne videoserien vår der vi har dokumentert selskapet siden
-                oppstarten med en ukentlig videoepisode.
+                {description}
               </h2>
             </div>
             <div className="flex mt-12 items-center justify-center sm:justify-start">
@@ -39,7 +40,7 @@ export const VideoHero = ({
                 </button>
               </span> */}
 
-              <Link to="/videoserie#miniserier">
+              <Link to="/videoserie">
                 <button
                   type="button"
                   aria-label="link"
@@ -49,13 +50,13 @@ export const VideoHero = ({
                 </button>
               </Link>
             </div>
-            <div className="twelve:absolute hidden twelve:block relative -mt-40 transform translate-x-28 w-7/12 right-0">
-              <FeaturedVideo
-                fallbackImg={fallbackImg}
-                video={video}
-                playlist={playlist}
-              />
-            </div>
+          </div>
+          <div className="twelve:absolute hidden twelve:block relative -mt-40 transform translate-x-28 w-7/12 right-40 -translate-y-10">
+            <FeaturedVideo
+              fallbackImg={fallbackImg}
+              video={video}
+              playlist={playlist}
+            />
           </div>
         </span>
       </div>
--- packages/website/src/templates/videoCategory.js
+++ packages/website/src/templates/videoCategory.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import Layout from '../layout';
+import { graphql } from 'gatsby';
 import { EpisodeCards } from '../components/episodeCards';
+import { VideoHero } from '../components/videoHero';
 import slugify from 'slugify';
 
-const VideoCategoryTemplate = ({ pageContext }) => {
+const VideoCategoryTemplate = ({ pageContext, data }) => {
   const { title, videos, slug } = pageContext;
   const [playlist, setPlaylist] = useState(null);
+  const image = data.allSanityVideoseries.nodes[0].heroImage.asset.fluid;
+  const { description } = data.allSanityVideoseries.nodes[0];
 
   useState(() => {
     const formattedVideos = videos.map((video) => {
@@ -26,7 +30,15 @@ const VideoCategoryTemplate = ({ pageContext }) => {
     <Layout>
       <div className="bg-navy text-white seven:px-10 overflow-hidden">
         <div className="max-w-1200 mx-auto">
-          <h1 className="text-3xl text-center my-10">{title}</h1>
+          <VideoHero
+            video={videos[0]}
+            backgroundImage={image}
+            fallbackImg={image}
+            title={title}
+            description={description}
+            playlist={playlist}
+          />
+          <div className="mt-5" />
           {playlist && <EpisodeCards playlist={playlist} tabs={false} />}
         </div>
       </div>
@@ -35,3 +47,34 @@ const VideoCategoryTemplate = ({ pageContext }) => {
 };
 
 export default VideoCategoryTemplate;
+
+export const query = graphql`
+  query {
+    allSanityVideoseries {
+      nodes {
+        description
+        featuredVideo
+        slug {
+          current
+        }
+        playlists {
+          process {
+            description
+            id
+            title
+          }
+        }
+        id
+        videoseriesTitle
+        heroImage {
+          asset {
+            url
+            fluid {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
