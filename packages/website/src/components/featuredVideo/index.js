d204f13d6d900fd7b793fce611e15c52e49ef677
Mikkel Eriksen Klokkerud <mikkel@mac.home>
2021-05-28 08:06:45 +0200

    resolve build error

--- packages/website/src/components/featuredVideo/index.js
+++ packages/website/src/components/featuredVideo/index.js
@@ -2,35 +2,35 @@ import React from 'react';
 import Link from 'gatsby-link';
 import * as Icon from '../icon';
 
-export const FeaturedVideo = ({ video, playlist }) => {
-  return (
-    <>
-      {video && (
-        <>
-          <div className="px-5">
-            <div className="w-12 h-2px sm:hidden bg-yellow mb-6 mt-6px" />
-          </div>
-        </>
-      )}
-      <Link
-        to={`/videoserie/${video.playlistSlug || 'videoserie'}/${video.slug}`}
-        state={{ playlist, video, id: video.videoId }}
-      >
-        <div
-          className="w-full pb-56c"
-          style={{
-            background:
-              'transparent linear-gradient(180deg, #1E92D000 0%, #061634 100%) 0% 0% no-repeat padding-box',
-            backgroundImage: video && `url("${video.thumbnails.standard.url}")`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
-        >
-          <div className="absolute right-5 bottom-5">
-            <Icon.VideoPlay color="#fff" />
-          </div>
+export const FeaturedVideo = ({ video, playlist }) => (
+  <>
+    {video && (
+      <>
+        <div className="px-5">
+          <div className="w-12 h-2px sm:hidden bg-yellow mb-6 mt-6px" />
         </div>
-      </Link>
-    </>
-  );
-};
+      </>
+    )}
+    <Link
+      to={`/videoserie/${video.playlistSlug || 'videoserie'}/${video.slug}`}
+      state={{ playlist, video, id: video.videoId }}
+    >
+      <div
+        className="w-full pb-56c"
+        style={{
+          background:
+            'transparent linear-gradient(180deg, #1E92D000 0%, #061634 100%) 0% 0% no-repeat padding-box',
+          backgroundImage:
+            video &&
+            `url("${video?.thumbnails?.standard?.url || video.thumbnail}")`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      >
+        <div className="absolute right-5 bottom-5">
+          <Icon.VideoPlay color="#fff" />
+        </div>
+      </div>
+    </Link>
+  </>
+);
