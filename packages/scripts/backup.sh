08fb2b8a9b14fba9314d7b01c5e12a2b017b3240
Tommy Mikalsen <tommy@mikalsen.it>
2021-06-08 21:35:23 +0200

    Add step to copy backup to Azure blob storage

--- packages/scripts/backup.sh
+++ packages/scripts/backup.sh
@@ -3,19 +3,29 @@
 set -e
 
 KEY_VAULT="alvnoprodkv"
+SUBSCRIPTION="AlvNo-Dev"
+STORAGE_ACCOUNT="alvnobackup"
+SAS_EXPIRY=$(date -u -v "+1H" +"%Y-%m-%dT%H:%M:%SZ")
 
 function getSecret() {
   az keyvault secret show --vault-name $KEY_VAULT --name $1 | jq '.value' -r
 }
 
+function getSasToken() {
+  az storage account generate-sas --permissions cw --account-name $1 --services b --resource-types sco --expiry $SAS_EXPIRY --subscription $2 | jq -r
+}
+
 export SANITY_AUTH_TOKEN="$(getSecret sanity-api-export-auth-token)"
 
-yarn
+SAS_TOKEN="$(getSasToken $STORAGE_ACCOUNT $SUBSCRIPTION)"
+DATETIME=$(date "+%Y-%m-%d_%H%M")
+BACKUP_FILE_DATE_NAME="$DATETIME-production.tar.gz"
+BACKUP_FILE_NAME="../cms/backupfolder/production.tar.gz"
+
+RM -f "$BACKUP_FILE_NAME"
 yarn workspace cms sanity dataset export production backupfolder
 
-DATETIME=$(date "+%Y-%m-%d_%H%M")
-BAKCUP_FILE_NAME="production.tar.gz"
-BAKCUP_FILE_DATE_NAME="$DATETIME-production.tar.gz"
+#Terraform?
+#azcopy make "https://alvnobackup.blob.core.windows.net/alvnobackup?$SAS_TOKEN"
 
-echo "$BAKCUP_FILE_DATE_NAME"
-# azcopy copy "backupfolder/$BAKCUP_FILE_NAME" "https://mystorageaccount.blob.core.windows.net/mycontainer/$BAKCUP_FILE_DATE_NAME"
+azcopy copy "$BACKUP_FILE_NAME" "https://alvnobackup.blob.core.windows.net/alvnobackup/$BACKUP_FILE_DATE_NAME?$SAS_TOKEN"
