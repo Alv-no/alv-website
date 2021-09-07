43a121977ca7c221965c0d3a04e4b8798fc39ce7
mikkelcodes <mikkelcodes@gmail.com>
2021-09-07 16:05:03 +0200

    Fix breadcrumbs - 'alv' to 'Alv'. Closes #440

--- packages/website/src/components/breadcrumb/index.jsx
+++ packages/website/src/components/breadcrumb/index.jsx
@@ -2,37 +2,35 @@ import React from 'react';
 import Link from 'gatsby-link';
 
 const Breadcrumb = ({ path }) => {
-  let crumbs = [];
-  const filteredPath = path.filter((el) => el.length > 0);
-  if (filteredPath) {
-    crumbs = filteredPath.map((el) => {
-      let formattedEl = el[0].toUpperCase() + el.slice(1);
-      if (formattedEl.includes('-')) {
-        if (path[0] !== 'vi-tilbyr' && path[0] !== 'videoserie') {
-          formattedEl.length > 35
-            ? (formattedEl = ['Blogginnlegg'])
-            : (formattedEl = formattedEl.split('-').join(' '));
-        } else {
-          formattedEl = formattedEl.split('-').join(' ');
+  const formatCrumbs = (crumbs) =>
+    crumbs
+      .filter((el) => el.length > 0)
+      .map((el) => {
+        let formattedEl = el[0].toUpperCase() + el.slice(1);
+        if (formattedEl.includes('-')) {
+          if (crumbs[0] === 'blogg' && formattedEl.length > 35) {
+            formattedEl = ['Blogginnlegg'];
+          } else {
+            formattedEl = formattedEl.split('-').join(' ');
+          }
         }
-      }
-      if (el === 'vi-tilbyr') formattedEl = 'Vi tilbyr';
-      return formattedEl;
-    });
-  }
-  crumbs.unshift('Hjem');
+        if (el === 'jobbe-i-alv') formattedEl = 'Jobbe i Alv';
+        return formattedEl;
+      });
+
+  const filteredPath = path.length ? formatCrumbs(path) : path;
+  filteredPath.unshift('Hjem');
   return (
     <div className="overflow-hidden">
-      {crumbs.length > 1 && (
+      {filteredPath.length > 1 && (
         <div className="flex space-x-4 text-white -ml-3 tracking-wider relative z-20">
-          {(crumbs || []).map((item, i) => {
+          {(filteredPath || []).map((item, i) => {
             let link;
             if (i === 0) {
               link = '/';
             } else {
               link = '/' + path.slice(0, i).join('/');
             }
-
             return (
               <div key={i}>
                 <Link to={link} className="last:font-bold font-extralight">
