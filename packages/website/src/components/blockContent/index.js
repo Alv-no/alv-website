a614113b9006fda9ff060e78a78473ed75d4d334
mikkelcodes <mikkelcodes@gmail.com>
2021-09-09 11:17:50 +0200

    Add white text prop and functionality. Closes #464

--- packages/website/src/components/blockContent/index.js
+++ packages/website/src/components/blockContent/index.js
@@ -3,8 +3,8 @@ import * as styles from './Blockcontent.module.css';
 import { richTextTypes } from '../richTextTypes';
 import PortableText from '@sanity/block-content-to-react';
 
-export const BlockContent = ({ blocks, noStyle }) => (
-  <span className={!noStyle && styles.body}>
+export const BlockContent = ({ blocks, noStyle, whiteText }) => (
+  <span className={!noStyle && (whiteText ? styles.whiteText : styles.body)}>
     <PortableText
       blocks={blocks}
       projectId="mnr37rl0"
