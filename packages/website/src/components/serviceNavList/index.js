f6087e012f9d5b2f90861d252be721553bf4326c
mikkelcodes <mikkelcodes@gmail.com>
2021-09-10 14:20:38 +0200

    Format nav links - category pages. Closes #476

--- packages/website/src/components/serviceNavList/index.js
+++ packages/website/src/components/serviceNavList/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as Icon from '../icon';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import { Title } from '../title';
 import { BlockContent } from '../blockContent';
@@ -6,17 +7,22 @@ import { BlockContent } from '../blockContent';
 export const ServiceNavList = ({ nav, heroImage, raw, heading, scrollTo }) => {
   return (
     <div className="px-5 sm:px-12 relative z-10" id="oversikt">
-      <div className="max-w-1200 mx-auto pt-10 sm:pt-16 w-full tracking-wider relative z-0">
+      <div className="max-w-1200 mx-auto pt-5 sm:pt-16 w-full tracking-wider relative z-0">
         <nav className="mx-auto sm:grid sm:gap-x-4 sm:grid-cols-navlist-sm">
-          <ul className="text-lg sm:mt-3 -mt-2 list-style-none text-navynav opacity-80 tracking-wider sm:block flex">
+          <ul className="text-lg sm:mt-20 -mt-2 list-style-none text-navynav opacity-80 tracking-wider block divide-y-2 divide-lightnavy divide-solid sm:pr-5 mb-3">
             {nav.map((el) => (
               <button
                 aria-label="Scroll Link"
-                className="mb-7 block sm:mr-0 mr-4"
+                className="py-3 block sm:mr-0 mr-4 w-full focus:outline-none"
                 onClick={scrollTo}
                 name={el.id}
               >
-                {el.label}
+                <span className="flex justify-between items-center">
+                  {el.label}
+                  <span className="scale-90 transform">
+                    <Icon.Arrow />
+                  </span>
+                </span>
               </button>
             ))}
           </ul>
@@ -30,12 +36,6 @@ export const ServiceNavList = ({ nav, heroImage, raw, heading, scrollTo }) => {
             className={`font-light block sm:mb-15 sm:h-auto -mb-10 relative overflow-hidden sm:overflow-visible`}
           >
             <div className="cursor-text text-left z-20 relative">
-              <div className="z-50 relative sm:block twelve:hidden mb-10 hidden">
-                <GatsbyImage
-                  image={heroImage}
-                  className="-mt-32 relative z-50 sm:-mr-12 sm:h-25vh md:h-30vh -mr-5"
-                />
-              </div>
               <div className="mb-10">
                 <Title
                   align="left sm:text-index"
