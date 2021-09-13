9c5f9113919fff129712ab23404b17ee0fcfbbfc
mikkelcodes <mikkelcodes@gmail.com>
2021-09-13 12:38:10 +0200

    Remove underline from nav. Closes #485

--- packages/website/src/components/servicesNav/index.js
+++ packages/website/src/components/servicesNav/index.js
@@ -5,16 +5,16 @@ export const ServicesNav = ({ nav, scrollTo }) => {
   return (
     <div className="max-w-1200 mx-auto py-10 mt-0 w-full">
       <nav className="mx-auto">
-        <ul className="sm:flex sm:justify-center justify-between mx-5 sm:mx-0 text-base sm:text-lg list-style-none p-0">
+        <ul className="sm:flex sm:justify-center justify-between divide-y-2 divide-lightnavy sm:divide-none mx-5 sm:mx-0 text-base sm:text-lg list-style-none p-0">
           {nav.map((el) => (
             <button
               aria-label="Scroll Link"
-              className="sm:mr-7 focus:outline-none w-full sm:w-auto justify-between flex items-center border-b-2 border-lightnavy pb-6px pt-1"
+              className="sm:mr-7 focus:outline-none w-full uppercase tracking-wider font-bold text-md sm:w-auto justify-between flex items-center pb-6px pt-1"
               onClick={scrollTo}
               name={el.id}
             >
               {el.label}
-              <span className="block ml-3 transform scale-80">
+              <span className="block ml-2 transform scale-80">
                 <Icon.Arrow />
               </span>
             </button>
