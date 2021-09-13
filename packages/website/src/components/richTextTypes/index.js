5756056a525da9b463e1dfbd11f8fe5924c190dd
mikkelcodes <mikkelcodes@gmail.com>
2021-09-13 13:44:45 +0200

    Add close/open functionality to video component. Closes #474

--- packages/website/src/components/richTextTypes/index.js
+++ packages/website/src/components/richTextTypes/index.js
@@ -8,6 +8,43 @@ const projectId = 'mnr37rl0';
 const dataset = 'production';
 const filePath = `https://cdn.sanity.io/files/${projectId}/${dataset}/`;
 
+const urlFor = (source) =>
+  imageUrlBuilder({ projectId, dataset }).image(source);
+
+const Image = ({ props, link }) => (
+  <a href={link} className={styles.wrapper}>
+    <div
+      alt={props.node.heading}
+      className={styles.img}
+      style={{
+        backgroundImage: `url(${urlFor(props.node.image.asset)
+          .width(700)
+          .url()})`,
+      }}
+    />
+  </a>
+);
+
+const TextSection = ({ props, link }) => (
+  <a href={link} className={styles.wrapper}>
+    <div className={styles.textWrapper}>
+      <div className="">
+        {props.node.heading && (
+          <p className={styles.heading}>{props.node.heading}</p>
+        )}
+        {props.node.description && (
+          <p className={styles.description}>{props.node.description}</p>
+        )}
+      </div>
+      {props.node.image && (
+        <div className={styles.buttonWrapper}>
+          <Button.CtaArrow>Learn More</Button.CtaArrow>
+        </div>
+      )}
+    </div>
+  </a>
+);
+
 const Video = ({ props, format }) => (
   <source
     src={`${filePath}${props.node.videoWebM.asset._ref.slice(
@@ -18,79 +55,47 @@ const Video = ({ props, format }) => (
   />
 );
 
-const urlFor = (source) =>
-  imageUrlBuilder({ projectId, dataset }).image(source);
-
-const Image = ({ props }) => (
-  <div
-    alt={props.node.heading}
-    className={styles.img}
-    style={{
-      backgroundImage: `url(${urlFor(props.node.image.asset)
-        .width(700)
-        .url()})`,
-    }}
-  />
-);
-
-const TextSection = ({ props }) => (
-  <div className={styles.textWrapper}>
-    <div className="">
-      {props.node.heading && (
-        <p className={styles.heading}>{props.node.heading}</p>
-      )}
-      {props.node.description && (
-        <p className={styles.description}>{props.node.description}</p>
-      )}
+const VideoSection = ({ props, link }) => (
+  <a href={link} className={styles.videoWrapper}>
+    <div className="relative">
+      <video controls autoplay="true" className={styles.video} muted>
+        {props.node.videoWebM && <Video props={props} format="webm" />}
+        {props.node.videoMp4 && <Video props={props} format="mp4" />}
+      </video>
     </div>
-    {props.node.image && (
-      <div className={styles.buttonWrapper}>
-        <Button.CtaArrow>Learn More</Button.CtaArrow>
-      </div>
-    )}
-  </div>
-);
-
-const VideoSection = ({ props }) => (
-  <div className="relative">
-    <video controls autoplay="true" className={styles.video} muted>
-      {props.node.videoWebM && <Video props={props} format="webm" />}
-      {props.node.videoMp4 && <Video props={props} format="mp4" />}
-    </video>
-  </div>
+  </a>
 );
 
 export const richTextTypes = {
   types: {
     cta: (props) => (
-      <div className="relative">
-        {!props.node.image && (
-          <>
-            <div className="absolute top-3 sm:right-18 right-3 transform scale-70 z-10 cursor-pointer">
-              <Icon.Cross />
-            </div>
-            <input
-              type="checkbox"
-              className="absolute top-1 sm:right-16 right-3 h-10 w-10 transform scale-70 z-10 opacity-0 cursor-pointer"
-            />
-          </>
-        )}
-        <a
-          href={props.node.link}
-          className={`${
-            props.node.image ? styles.wrapper : styles.videoWrapper
-          }`}
-        >
+      <div className={styles.ctaContainer}>
+        {!props.node.image && <ShowMediaToggle />}
+        <span className={styles.mediaWrapper}>
           {props.node.videoWebM || props.node.videoMp4 ? (
-            <VideoSection props={props} />
+            <VideoSection props={props} link={props.node.link} />
           ) : (
-            <Image props={props} />
+            <Image props={props} link={props.node.link} />
           )}
-          {props.node.description || props.node.heading ? (
-            <TextSection props={props} />
-          ) : null}
-        </a>
+        </span>
+        {props.node.description || props.node.heading ? (
+          <TextSection props={props} link={props.node.link} />
+        ) : null}
       </div>
     ),
   },
 };
+
+const ShowMediaToggle = () => (
+  <>
+    <input type="checkbox" className={styles.checkbox} />
+    <div className={styles.iconWrapper}>
+      <div className={styles.crossIcon}>
+        <Icon.Cross />
+      </div>
+      <div className={styles.expandIcon}>
+        <Icon.DropdownArrow />
+      </div>
+    </div>
+  </>
+);
