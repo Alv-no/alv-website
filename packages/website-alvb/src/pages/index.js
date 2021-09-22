f279a7dfdcee02a4300d547977fb63aa54254852
mikkelcodes <mikkelcodes@gmail.com>
2021-09-22 16:18:57 +0200

    Create hero component. Closes #507

--- packages/website-alvb/src/pages/index.js
+++ packages/website-alvb/src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import { Layout } from '../components/layout';
 import { BlogCarousel } from '../components/blogCarousel';
+import { Hero } from '../components/hero';
 import { WhoWeAre, VideoIntro, Hire } from 'shared-components';
 
 const Index = ({ data }) => {
@@ -11,34 +12,36 @@ const Index = ({ data }) => {
 
   return (
     <div className="overflow-hidden">
-      {data?.sanityLandingPage && (
-        <Layout white pageTitle={pageTitle} pageDescription={pageDescription}>
-          <div className="bg-theme-bg">
-            <div className="bg-theme-bg w-full pb-15 sm:pt-0 pt-10">
-              <VideoIntro
-                videoMp4={landingPage.videoMp4.asset.url}
-                videoWebm={landingPage.videoWebm.asset.url}
-              >
-                {landingPage.videoTextOverlay}
-              </VideoIntro>
-              <WhoWeAre
-                title={landingPage.section2Title}
-                darkText
-                blocks={landingPage._rawSection2Text}
-              />
-            </div>
-            <Hire
-              blue={true}
-              title={landingPage.section3Title}
-              text={landingPage.section3Text}
-              image={landingPage.section3Image.asset.gatsbyImageData}
+      <Layout white pageTitle={pageTitle} pageDescription={pageDescription}>
+        <div className="bg-theme-bg">
+          <Hero
+            blocks={landingPage.imageHero._rawHeroText}
+            image={landingPage.imageHero.image.asset.gatsbyImageData}
+          />
+          <div className="bg-theme-bg w-full pb-15 sm:pt-24">
+            <VideoIntro
+              videoMp4={landingPage.videoMp4.asset.url}
+              videoWebm={landingPage.videoWebm.asset.url}
+            >
+              {landingPage.videoTextOverlay}
+            </VideoIntro>
+            <WhoWeAre
+              title={landingPage.section2Title}
+              darkText
+              blocks={landingPage._rawSection2Text}
             />
-            <div className="bg-theme-bg h-10 lg:h-32" />
-            <div className="lg:h-40 h-5" />
-            <BlogCarousel blue />
           </div>
-        </Layout>
-      )}
+          <Hire
+            blue={true}
+            title={landingPage.section3Title}
+            text={landingPage.section3Text}
+            image={landingPage.section3Image.asset.gatsbyImageData}
+          />
+          <div className="bg-theme-bg h-10 lg:h-32" />
+          <div className="lg:h-40 h-5" />
+          <BlogCarousel blue />
+        </div>
+      </Layout>
     </div>
   );
 };
@@ -74,7 +77,7 @@ export const query = graphql`
       imageHero {
         image {
           asset {
-            gatsbyImageData
+            gatsbyImageData(placeholder: BLURRED)
           }
         }
         _rawHeroText
