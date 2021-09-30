3e9044cfedd7b0765a9379644bd561e6b61fc158
mikkelcodes <mikkelcodes@gmail.com>
2021-09-30 16:28:06 +0200

    add blogg page

---
+++ packages/website-alvb/src/pages/kunngjøringer-og-blogg.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Layout } from '../components/layout';
+import { Title, IntroContainer, BlogSection } from 'shared-components';
+import { useBlogQuery } from '../hookspages/useBlogQuery';
+
+const Blog = () => {
+  const data = useBlogQuery();
+  const articles = data.articles.edges
+    .map((article) => article.node)
+    .sort((a, b) => (a.rawDate > b.rawDate ? -1 : 1));
+  const featuredArticle = articles[0];
+  articles.shift();
+  return (
+    <Layout
+      pageTitle={data.sanityBlogPage.pageTitle}
+      pageDescription={data.sanityBlogPage.pageDescription}
+    >
+      <div className="overflow-hidden">
+        <IntroContainer article={featuredArticle}>
+          <Title align="text-center twelve:text-left">Blogg</Title>
+          <div className="w-full text-center">
+            <div className="text-white tracking-wider text-blog font-light mt-8 twelve:w-full eight:w-1/2 w-5/6 mx-auto twelve:mx-0 twelve:text-left">
+              Vi vil gjerne dele litt av vår{' '}
+              <span className="font-semibold">kunnskap</span>, og gi deg noen av
+              våre <span className="font-semibold">tips og tanker</span>
+            </div>
+          </div>
+        </IntroContainer>
+        <BlogSection allArticles={articles} />
+      </div>
+    </Layout>
+  );
+};
+
+export default Blog;
