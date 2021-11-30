2af11f84e6341fa35ba177c16861fa7bf16a4408
Frank Lyder Bredland <fl@nyggen.com>
2021-11-30 21:28:50 +0100

    Fix CallToAction-test

--- packages/shared-components/src/components/calltoaction/calltoaction.spec.js
+++ packages/shared-components/src/components/calltoaction/calltoaction.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import CallToAction from '.';
+import { CallToAction } from '.';
 
 describe('CallToAction-component', () => {
   it('should render', () => {
