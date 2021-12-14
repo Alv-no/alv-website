4dcad772d858935a8420bc1e7bfa46a04c2df9a3
Mikkel Eriksen Klokkerud <mikkel@Mikkels-MacBook-Pro.local>
2021-12-14 01:35:16 +0100

    move nav back to shared

--- packages/website-alvb/src/utils/navItems.js
+++ packages/website-alvb/src/utils/navItems.js
@@ -2,21 +2,21 @@ export const navItems = [
   {
     no: {
       text: 'Våre prosjekter',
-      link: '/våre-prosjekter',
+      link: '/våre-prosjekter/',
     },
     en: {
       text: 'Our projects',
-      link: '/en/our-projects',
+      link: '/en/our-projects/',
     },
   },
   {
     no: {
       text: 'Biobank',
-      link: '/biobank',
+      link: '/biobank/',
     },
     en: {
       text: 'Biobank',
-      link: '/en/biobank',
+      link: '/en/biobank/',
     },
   },
   {
@@ -25,25 +25,25 @@ export const navItems = [
       children: [
         {
           text: 'Investering',
-          link: '/samarbeid-med-oss/investering',
+          link: '/samarbeid-med-oss/investering/',
         },
       ],
     },
     en: {
       text: 'Collaborate with us',
-      children: [{ text: 'Invest', link: '/en/collaborate-with-us/invest' }],
+      children: [{ text: 'Invest', link: '/en/collaborate-with-us/invest/' }],
     },
   },
   {
-    no: { text: 'Vårt team', link: '/vårt-team' },
-    en: { text: 'Our team', link: '/en/our-team' },
+    no: { text: 'Vårt team', link: '/vårt-team/' },
+    en: { text: 'Our team', link: '/en/our-team/' },
   },
   {
-    no: { text: 'Kunngjøringer og blogg', link: '/blogg' },
-    en: { text: 'Announcements and blog', link: '/en/blog' },
+    no: { text: 'Kunngjøringer og blogg', link: '/blogg/' },
+    en: { text: 'Announcements and blog', link: '/en/blog/' },
   },
   {
-    no: { text: 'Kontakt oss', link: '/kontakt-oss' },
-    en: { text: 'Contact us', link: '/en/contact-us' },
+    no: { text: 'Kontakt oss', link: '/kontakt-oss/' },
+    en: { text: 'Contact us', link: '/en/contact-us/' },
   },
 ];
