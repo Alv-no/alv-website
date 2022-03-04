41a0b9eeee22673eb736065f697ba7df47ad23db
Martin Klingenberg <martin.klingenberg@alv.no>
2022-03-04 09:14:18 +0100

    career-pages SSR-ified

--- packages/website/src/pages/jobbe-i-alv.js
+++ packages/website/src/pages/jobbe-i-alv.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Layout from '../components/layout';
+import { client } from '../server-side/client';
+import { createGatsbyImages } from '../server-side/imageCreator';
 import { ImageTextListHero } from '../components/imageTextHero';
 import { ReasonsSlider } from '../components/reasonsSlider';
-import { useWorkQuery } from '../hookspages/useWorkQuery';
 import { useLayoutQuery } from '../hooks/useLayoutQuery';
+import { gql } from '@apollo/client';
+import { graphql } from 'gatsby';
 
-const WorkForAlv = () => {
-  const { sanityCareerPage, stairs, street } = useWorkQuery();
+const WorkForAlv = ({ data, serverData }) => {
+  const { stairs, street } = data;
+  const sanityCareerPage = serverData.careerData.allCareerPage[0];
+
   const layoutData = useLayoutQuery();
-
   return (
     <>
       <Layout
@@ -39,3 +43,80 @@ const WorkForAlv = () => {
 };
 
 export default WorkForAlv;
+
+export const query = graphql`
+  {
+    stairs: file(name: { eq: "stairs" }) {
+      childImageSharp {
+        gatsbyImageData(layout: FULL_WIDTH)
+      }
+    }
+    street: file(name: { eq: "jobbeialv_1" }) {
+      childImageSharp {
+        gatsbyImageData(layout: FULL_WIDTH)
+      }
+    }
+  }
+`;
+
+async function getCareerPageServerSide() {
+  const response = await client.query({
+    query: gql`
+      {
+        allCareerPage {
+          pageDescription
+          pageTitle
+          positionsListLeft {
+            slug {
+              current
+            }
+            pageTitle
+          }
+          positionsListRight {
+            pageTitle
+            slug {
+              current
+            }
+          }
+          reasonsCarousel {
+            mainHeading
+            process {
+              heading
+              description
+              image {
+                asset {
+                  id: _id
+                  metadata {
+                    dimensions {
+                      height
+                      width
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+    fetchPolicy: 'no-cache',
+  });
+  createGatsbyImages(response.data);
+  return response.data;
+}
+
+export async function getServerData() {
+  try {
+    const careerData = await getCareerPageServerSide();
+    return {
+      props: {
+        careerData,
+      },
+      status: 200,
+    };
+  } catch {
+    return {
+      status: 500,
+    };
+  }
+}
