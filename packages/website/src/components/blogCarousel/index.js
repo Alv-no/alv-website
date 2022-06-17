00a9ad8c012050d09bc1a8b82e57173f90c0719b
Mikkel Eriksen Klokkerud <@mikkelklokkerud.elaway.io>
2022-06-17 11:12:43 +0200

    update cta bg

--- packages/website/src/components/blogCarousel/index.js
+++ packages/website/src/components/blogCarousel/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { BlogSlider } from 'shared-components';
 import { useBlogQuery } from '../../hooks/useBlogQuery';
 
-export const BlogCarousel = ({ blue }) => (
-  <BlogSlider blue={blue} useBlogQuery={useBlogQuery} />
+export const BlogCarousel = ({ blue, blueText }) => (
+  <BlogSlider blue={blue} blueText={blueText} useBlogQuery={useBlogQuery} />
 );
--- packages/website/src/components/ctaSection/index.js
+++ packages/website/src/components/ctaSection/index.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
-export const CtaSection = ({ eyebrow, heading, buttonText, internalLink }) => {
+export const CtaSection = ({
+  eyebrow,
+  heading,
+  buttonText,
+  internalLink,
+  navy,
+}) => {
   return (
-    <section className="px-10 flex justify-center items-center py-10 flex-col bg-servicesgray mt-4">
-      <p className="font-light text-center font-lg mb-3">{eyebrow}</p>
-      <h4 className="font-semibold text-center text-cta-section mb-4">
+    <section
+      className={`px-10 flex justify-center items-center flex-col ${
+        navy ? 'bg-navy text-white' : 'bg-servicesgray'
+      }`}
+    >
+      <p className="font-light text-center font-lg mb-2">{eyebrow}</p>
+      <h4 className="font-semibold text-center text-cta-section mb-5">
         {heading}
       </h4>
       <Link to={internalLink}>
         <button
           type="button"
           aria-label={buttonText}
-          className="px-10 rounded rounded-full font-semibold text-base uppercase py-6px border border-navy border-2"
+          className={`px-10 rounded rounded-full font-semibold text-base uppercase py-6px border ${
+            navy ? 'border-white' : ' border-navy'
+          } border-2`}
         >
           {buttonText}
         </button>
