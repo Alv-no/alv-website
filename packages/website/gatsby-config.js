faac65dffba254c23aab0e0f63e4833d2980cfe4
Martin Klingenberg <martin.klingenberg@alv.no>
2022-08-18 12:04:37 +0200

    Added fastify to the project. But ssr-dynamic routes won't work

--- packages/website/gatsby-config.js
+++ packages/website/gatsby-config.js
@@ -15,6 +15,13 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-fastify`,
+      options: {
+        /* discussed below */
+        features: {},
+      },
+    },
+    {
       resolve: `gatsby-plugin-gdpr-cookies`,
       options: {
         googleAnalytics: {
