644c6581fdb913f49537654f61dd74107eb964f1
Frank Lyder Bredland <fl@nyggen.com>
2022-11-12 13:58:17 +0100

    Remove react-three-fiber experiment

--- packages/website/src/components/smoke/index.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Smoke from './index';
-
-export default {
-  title: 'components/smoke',
-};
-
-export const Default = ({ ...args }) => <Smoke {...args} />;
-Default.args = {};
