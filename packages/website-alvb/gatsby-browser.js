cae96a5a2863ce0c355892f8fc7898e6d8dcbe82
Mikkel K <mikkelcodes@gmail.com>
2022-11-22 16:51:27 +0100

    remove unused font weight import

--- packages/website-alvb/gatsby-browser.js
+++ packages/website-alvb/gatsby-browser.js
@@ -2,7 +2,6 @@ import '@fontsource/source-sans-pro/200.css';
 import '@fontsource/source-sans-pro/300.css';
 import '@fontsource/source-sans-pro/400.css';
 import '@fontsource/source-sans-pro/700.css';
-import '@fontsource/source-sans-pro/900.css';
 
 import './src/components/layout/layout.css';
 export const onClientEntry = () => {
