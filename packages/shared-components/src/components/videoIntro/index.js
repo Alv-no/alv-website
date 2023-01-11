d6fe777221ef88b7525839c3932e60fabcb195fe
Mikkel K <mikkelcodes@gmail.com>
2023-01-11 08:24:41 +0100

    reintroduce video component to landing page

--- packages/shared-components/src/components/videoIntro/index.js
+++ packages/shared-components/src/components/videoIntro/index.js
@@ -4,25 +4,28 @@ import Fade from 'react-reveal/Fade';
 export const VideoIntro = ({ videoMp4, videoWebm, children }) => {
   return (
     <div
-      className="lg:grid mx-auto text-theme-text relative justify-center lg:mr-12"
-      style={{ gridTemplateColumns: 'minmax(400px, 866px) 167px' }}
+      className="lg:pr-12 lg:grid mx-auto sm:pt-15 text-theme-text items-center"
+      style={{ gridTemplateColumns: '70% auto' }}
     >
-      <div className="relative w-full" style={{ maxWidth: '866px' }}>
-        <video className="h-auto mx-auto" controls autoPlay={true} muted>
+      <div className="relative lg:pr-4 w-full">
+        <video
+          className="h-auto mx-auto"
+          controls
+          autoplay="true"
+          muted
+          defaultMuted
+        >
           <source src={videoWebm} type="video/webm" />
           <source src={videoMp4} type="video/mp4" />
         </video>
       </div>
       <Fade>
-        <h2
-          className="uppercase hidden lg:block top-0 right-0 text-cta-lg font-semibold tracking-wider leading-tight relative -ml-32 top-1/6"
-          style={{ width: '334px', top: '23%' }}
-        >
+        <h2 className="uppercase hidden lg:block top-0 right-0 lg:text-3xl xl:text-cta-lg font-semibold tracking-wider transform translate-x-20 leading-tight mt-15 lg:mt-0 lg:w-5/6 lg:pl-5">
           {children}
         </h2>
       </Fade>
       <Fade>
-        <h2 className="uppercase block lg:hidden text-index sm:text-slider font-semibold tracking-wider leading-tight mt-5 sm:mt-10">
+        <h2 className="uppercase block lg:hidden text-index sm:text-slider font-semibold tracking-wider sm:w-9/12 leading-tight mt-5 sm:mt-10">
           {children}
         </h2>
       </Fade>
