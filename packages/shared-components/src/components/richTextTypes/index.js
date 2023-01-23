e2bd7333dc6793dadb3dfefd9dca3b41408761f5
Mikkel K <mikkelcodes@gmail.com>
2023-01-23 15:19:26 +0100

    cta update

--- packages/shared-components/src/components/richTextTypes/index.js
+++ packages/shared-components/src/components/richTextTypes/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import {
-  Cta,
-  ImageTextFlip,
-  LinkableHeading,
-  ProductCta,
-  TextCta,
-} from './types';
+import { ImageTextFlip, LinkableHeading, ProductCta, TextCta } from './types';
+
+// create combined CTA
 
 export const richTextTypesSerializer = (config) => ({
   types: {
-    cta: (props) => <Cta {...props.node} config={config} />,
     textCta: (props) => <TextCta {...props.node} config={config} />,
     productCta: (props) => <ProductCta {...props.node} config={config} />,
     imageTextFlip: (props) => <ImageTextFlip {...props.node} config={config} />,
@@ -20,9 +15,7 @@ export const richTextTypesSerializer = (config) => ({
     // for backwards compatibility
     // headingDescButtonCta can be safely removed after updating serialised data with a sanity script that replaces "headingDescButtonCta" with "textCta"
     // see https://www.sanity.io/docs/migrating-data for more info
-    headingDescButtonCta: (props) => (
-      <Cta ctaType="text" textSection={{ ...props.node }} />
-    ),
+    headingDescButtonCta: (props) => <TextCta {...props.node} />,
   },
   marks: {
     'alv-yellow': ({ children }) => (
