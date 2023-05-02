e05c1e616aa465692c2c70568a3e5b5f21065d37
Frank Lyder Bredland <fl@nyggen.com>
2023-05-02 09:52:19 +0200

    Css changes, and change default port of video-party

--- packages/video-party/src/pages/_app.js
+++ packages/video-party/src/pages/_app.js
@@ -4,6 +4,7 @@ import { Source_Sans_Pro } from "next/font/google";
 const sourceSansPro = Source_Sans_Pro({
   subsets: ["latin"],
   weight: ["200", "300", "400", "600", "700", "900"],
+  fallback: ["sans-serif"],
 });
 
 export default function App({ Component, pageProps }) {
