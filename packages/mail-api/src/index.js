73b2342dc5b89dd7407a97fe69d29c5241655c1a
Sofie <97192110+sofieborchgrevink@users.noreply.github.com>
2023-08-21 16:23:04 +0200

    Airtable integration (#953)
    * added filename to url
    
    * removed deletion of file and shortened filename
    
    * files to file in url
    
    * test
    
    * added logging with correlationid etc
    
    * package
    
    * added viruscheck and fixed comment
    
    * ..
    
    * ..
    
    * ..
    
    * moved cors

--- packages/mail-api/src/index.js
+++ packages/mail-api/src/index.js
@@ -5,10 +5,10 @@ const app = express();
 const port = 80;
 const routes = require("./routes");
 
+app.use(cors());
 app.use(routes);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 
 app.listen(port, () => {
   console.log("Listening on port " + port);
