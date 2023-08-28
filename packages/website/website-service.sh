cb69c2fc8de3026dd8e89946d73a4c649dd09deb
Fredrik Jenssen <fredrik.jenssen@alv.no>
2023-08-28 15:30:31 +0200

    957: move website scripts into own file, use it in run script, create git hub action draft

---
+++ packages/website/website-service.sh
@@ -0,0 +1,56 @@
+# Relies on:
+# - docker
+function buildWebsite() {
+  ENV=$1
+  SANITY_TOKEN=$2
+  YT_API=$3
+  CONTAINER_REGISTRY=$4
+  TAG=$5
+
+  echo "building alv website"
+
+  if [ ! -f .env.production ]; then
+    echo "ENV=$ENV" >>.env.production
+    echo "SANITY_TOKEN=$SANITY_TOKEN" >>.env.production
+    echo "YT_API=$YT_API" >>.env.production
+    if [ "$ENV" != "prod" ]; then
+      echo "SANITY_DATASET=development" >>.env.production
+    fi
+  fi
+
+  docker build . \
+    --file website.Dockerfile \
+    --tag "$CONTAINER_REGISTRY/website:$TAG" \
+    --no-cache
+}
+
+# Relies on:
+# - docker
+function pushWebsite() {
+  CONTAINER_REGISTRY=$1
+  TAG=$2
+
+  echo "pushing website"
+
+  docker push "$CONTAINER_REGISTRY/website:$TAG"
+}
+
+# Relies on:
+# - helm
+function deployWebsite() {
+  ALV_HOSTNAME=$1
+  CONTAINER_REGISTRY=$2
+  TAG=$3
+
+  echo "helm website"
+
+  CHART_NAME_SUFFIX=''
+  HOSTS="$ALV_HOSTNAME,www.$ALV_HOSTNAME"
+
+  helm upgrade "website$CHART_NAME_SUFFIX" ./chart \
+    --install \
+    --set image.repository="$CONTAINER_REGISTRY/website" \
+    --set image.tag="$TAG" \
+    --set "hosts={$HOSTS}" \
+    --namespace "alv-no"
+}
