7b60d5265b324206c9c2fe3690bad77b004b7025
Fredrik Jenssen <fredrik.jenssen@alv.no>
2023-09-01 12:53:30 +0200

    957: try to use same azure service provider credentials

--- bash-services/helm-service.sh
+++ bash-services/helm-service.sh
@@ -1,12 +1,12 @@
 # Relies on:
 # - helm
 function helmLoginAndPull() {
-  SP_ALVTIME_ADMIN_CLIENT_ID=$1
-  SP_ALVTIME_ADMIN_RBAC_SECRET=$2
+  SERVICE_PROVIDER_CLIENT_ID=$1
+  SERVICE_PROVIDER_SECRET=$2
   CONTAINER_REGISTRY=$3
   
-  echo "$SP_ALVTIME_ADMIN_RBAC_SECRET" | helm registry login "$CONTAINER_REGISTRY" \
-    --username "$SP_ALVTIME_ADMIN_CLIENT_ID" \
+  echo "$SERVICE_PROVIDER_SECRET" | helm registry login "$CONTAINER_REGISTRY" \
+    --username "$SERVICE_PROVIDER_CLIENT_ID" \
     --password-stdin
 
   if [[ -d "chart" ]] && [[ "$CI" ]]; then
