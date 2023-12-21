b07fd41712a57c1fa08fd36eb415716fae205d20
Martin Klingenberg <martin.klingenberg@alv.no>
2023-12-21 13:55:02 +0100

    Small bug fix

--- packages/mail-api/src/index.js
+++ packages/mail-api/src/index.js
@@ -5,7 +5,7 @@ import routes from "./routes.js";
 import { loggerMiddleware } from "./logger.js";
 
 const app = express();
-const port = 8083;
+const port = 80;
 
 app.use(cors());
 app.use(loggerMiddleware);
